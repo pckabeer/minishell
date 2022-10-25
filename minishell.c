263324064646fa720f7b5aee528f74bf4fe195d5
pckabeer <pckabeer@gmail.com>
2022-10-25 11:41:41 +0400

    inital setups

--- minishell.c
+++ minishell.c
@@ -1,20 +1,51 @@
 #include "minishell.h"
 
-int parse()
-int main()
+// int parse()
+void cleanexit()
 {
-    char *inpt;
-
+    exit();
+}
+char *ft_readmsv.rline()
+{
+    
+}
+void init_minishell(t_msvar *msv)
+{
+    msv->rline = NULL;
+    msv->rline_split = NULL;
+}
+/*
+    The function read_loop  reads line from the command line and then processes it 
+*/
+int read_loop(t_msvar *msv)
+{
     while(1)
     {
-
-        inpt = readline("minishell ");
-        add_history(inpt);
-       if(!strcmp(inpt,"exit"))
-            exit(0);
-        printf("%s", inpt);
-        printf("\n");
+        msv->rline = readline("minishell$");
+        if(msv->rline)
+        {
+            add_history(msv->rline);
+            msv->rline_split = ft_split(msv->rline);
+        }
+        else
+		{
+			ft_putstr_fd("exit\n", 2);
+			cleanexit();
+		}
+        while(msv->rline_split)
+        
+      
+        free(msv->rline);
     }
+}
+int main()
+{
+    t_msvar msv;
+
+    init_minishell(&msv);
+    read_loop(&msv);
+
+
     return 0;
 }
 
--- minishell.h
+++ minishell.h
@@ -1,6 +1,18 @@
+#ifndef PHILO_H
+# define PHILO_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
+
+typedef struct minishellvariables
+{
+    char *rline;
+    char **rline_split;
+}t_msvar;
+char	**ft_split(char const *s, char c);
+
+#endif
