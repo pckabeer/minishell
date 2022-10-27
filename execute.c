d809e1724593f5ad701583ac1a04a00d35c7a6c8
pckabeer <pckabeer@gmail.com>
2022-10-27 23:16:19 +0400

    execute file added

---
+++ execute.c
@@ -0,0 +1,13 @@
+#include "minishell.h"
+
+void ft_exec_echo(t_msvar *msv)
+{
+    ft_putstr_fd(msv->rline+5, 1);
+    ft_putchar_fd('\n',1);
+}
+
+void  ft_exec(t_msvar *msv)
+{
+    if(msv->cmd_num == 1)
+        ft_exec_echo(msv);
+}
